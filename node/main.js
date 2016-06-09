54d7bf6ec79edc5c7b5e2b53583873a85921eb54
= <aranlong@live.co.uk>
2016-06-09 13:33:57 +0100

    Added http get request for incident table from service now

--- node/main.js
+++ node/main.js
@@ -1,14 +1,16 @@
 var fs = require("fs");//file reader
 var http = require("http");
+var incidentRequestURL = "https://orgen-jun7dlh005.lab.service-now.com/api/now/table/incident";
 
 http.createServer(function (request, response) {
-
    // Send the HTTP header 
    // HTTP Status: 200 : OK
-   // Content Type: text/plain
+   // Content Type: text/html
    response.writeHead(200, {'Content-Type': 'text/html'});
    
    var data = fs.readFileSync('index.html');
+   data = getIncidentTable();
+   
    response.end(data);//'<h1>Hello World</h1>\n');
 
 
@@ -16,3 +18,24 @@ http.createServer(function (request, response) {
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
+
+function getIncidentTable(){
+	var requestBody = ""; 
+
+	var client=new XMLHttpRequest();
+	client.open("get","https://orgen-jun7dlh005.lab.service-now.com/api/now/table/incident");
+
+	client.setRequestHeader('Accept','application/json');
+	client.setRequestHeader('Content-Type','application/json');
+
+	//Eg. UserName="admin", Password="admin" for this code sample.
+	client.setRequestHeader('Authorization', 'Basic '+btoa('admin'+':'+'admin'));
+
+	client.onreadystatechange = function() { 
+		if(this.readyState = this.DONE) {
+			//document.getElementById("response").innerHTML=this.status + this.response; 
+			return this.response;
+		}
+	}; 
+	client.send(requestBody);
+}
