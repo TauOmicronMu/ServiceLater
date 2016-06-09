f74c7a96bc2652c517136a856ad88f2206299ca1
= <aranlong@live.co.uk>
2016-06-09 12:00:36 +0100

    Fixed ip

--- node/main.js
+++ node/main.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('<h1>Hello World</h1>\n');
-}).listen(8080, 'localhost');
-console.log('Server running at http://localhost:8080/');
+}).listen(8080, '127.0.0.1');
+console.log('Server running at http://178.62.82.239:8080/');
