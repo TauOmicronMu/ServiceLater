bec9ca93902ca7fccde9d944f8b217f7cb0bd976
Reece Bennett <reece.bennett1@gmail.com>
2016-06-10 12:10:59 +0100

    IT WORKS!

--- server.js
+++ server.js
@@ -3,15 +3,25 @@ var app = express();
 var fs = require("fs");
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var btoa = require("btoa");
+var formidable = require("formidable");
 
 var j;
 var bodyOut = "";
 
-app.get('/', function (req, res) {
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.get('/signedIn', function (req, res) {
   var requestBody = "";
 
+  console.log(req.query.user);
+  var name = req.query.user;
+
   var client = new XMLHttpRequest();
-  client.open("get","https://orgen-jun7dlh005.lab.service-now.com/api/x_snc_twitter_inci/incidentretrieval/data?id=@Genius");
+  client.open("get","https://orgen-jun7dlh005.lab.service-now.com/api/x_snc_twitter_inci/incidentretrieval/data?id=@" + name);
 
   client.onreadystatechange = function() { 
     if(client.readyState == 4 && client.status == 200) {
@@ -19,7 +29,7 @@ app.get('/', function (req, res) {
       j = JSON.parse(client.responseText);
       console.log(j);
 
-      bodyOut = "<ul>"
+      bodyOut = "<ul style=\'list-style-type: none; padding: 10px; font-family: helvetica;\'>"
 
       for (var i in j.result) {
         var course = j.result[i].course || "";
@@ -27,7 +37,7 @@ app.get('/', function (req, res) {
         var priority = j.result[i].priority;
         var description = j.result[i].description;
         console.log("Course: " + course + " Dep: " + department + " Priority: " + priority + " Desc: " + description);
-        bodyOut += "<li>" + "Course: " + course + " Dep: " + department + " Priority: " + priority + " Desc: " + description + "</li>";
+        bodyOut += "<li style=\'margin-bottom: 20px;\'>" + "<b>Course:</b> " + course + " <b>Dep:</b> " + department + " <b>Priority:</b> " + priority + " <b>Desc:</b> " + description + "</li>";
       }
 
       bodyOut += "</ul>";
